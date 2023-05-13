44bceeb64fc5624b03d8e8c716a2bb79718fcc97
raerae04 <hudson.lavida@gmail.com>
2023-05-13 17:20:06 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,78 +1,90 @@
 import pygame
+import time
+import os
 
-# Initialize Pygame
+# Initialize
 pygame.init()
 
-# Set screen width and height
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+# Current Directory
+current_dir = os.path.dirname(__file__)
+print(current_dir)
+image_dir = os.path.join(current_dir, "images")
+print(image_dir)
 
-# Set window title
+# Set screen dimensions and caption
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("My Game")
 
-# Define button sizes and colors
-button_width = 200
-button_height = 50
-button_color = (255, 0, 255)
-button_active_color = (200, 0, 200)
+# Load image files
+background_image = pygame.image.load(os.path.join(image_dir, "scene1.jpg"))
+character_image = pygame.image.load(os.path.join(image_dir, "icon.png"))
 
-# Create play button rectangle
-play_button_rect = pygame.Rect((screen_width/2 - button_width/2), (screen_height/2 - button_height/2 - 100), button_width, button_height)
+# Define button dimensions and positions
+BUTTON_WIDTH = 150
+BUTTON_HEIGHT = 50
+BUTTON_MARGIN = 20
+play_button_pos = (SCREEN_WIDTH//2 - BUTTON_WIDTH//2, SCREEN_HEIGHT//2 - BUTTON_HEIGHT//2 - BUTTON_HEIGHT - BUTTON_MARGIN)
+settings_button_pos = (SCREEN_WIDTH//2 - BUTTON_WIDTH//2, SCREEN_HEIGHT//2 - BUTTON_HEIGHT//2)
+credits_button_pos = (SCREEN_WIDTH//2 - BUTTON_WIDTH//2, SCREEN_HEIGHT//2 - BUTTON_HEIGHT//2 + BUTTON_HEIGHT + BUTTON_MARGIN)
 
-# Create settings button rectangle
-settings_button_rect = pygame.Rect((screen_width/2 - button_width/2), (screen_height/2 - button_height/2), button_width, button_height)
+def main_menu():
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button_rect.collidepoint(event.pos):
+                    game_loop()
 
-# Create credits button rectangle
-credits_button_rect = pygame.Rect((screen_width/2 - button_width/2), (screen_height/2 - button_height/2 + 100), button_width, button_height)
+        # Draw buttons and labels
+        screen.blit(play_button_img, play_button_pos)
+        screen.blit(settings_button_img, settings_button_pos)
+        screen.blit(credits_button_img, credits_button_pos)
 
-# Set font and font size for button labels
-font_size = 24
-font = pygame.font.Font(None, font_size)
+        # Update the display
+        pygame.display.update()
 
-try:
-	# Game loop
-	running = True
-	print("Game is running")
-	while running:
-		# Handle events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
+def game_loop():
+    # Set starting character position and timer
+    character_pos = (0, -100)
+    character_timer = 0
 
-			# Handle mouse click event on button surfaces
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if event.button == 1:
-					if play_button_rect.collidepoint(event.pos):
-						print("Play button clicked")
-					elif settings_button_rect.collidepoint(event.pos):
-						print("Settings button clicked")
-					elif credits_button_rect.collidepoint(event.pos):
-						print("Credits button clicked")
+    # Run the game loop
+    running = True
+    while running:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Start the character timer when the user clicks the play button
+                if play_button_rect.collidepoint(event.pos):
+                    character_timer = pygame.time.get_ticks() + 5000
 
-		# Draw buttons to screen
-		pygame.draw.rect(screen, button_color, play_button_rect)
-		pygame.draw.rect(screen, button_color, settings_button_rect)
-		pygame.draw.rect(screen, button_color, credits_button_rect)
+        # Draw the scene
+        screen.blit(background_image, (0, 0))
+        if pygame.time.get_ticks() > character_timer:
+            screen.blit(character_image, character_pos)
+            character_pos = (200, 200) # change this to whatever position you want
 
-		# Add button labels to screen
-		play_button_text = font.render("Play", True, (255, 255, 255))
-		screen.blit(play_button_text, (play_button_rect.x + button_width/2 - play_button_text.get_width()/2, play_button_rect.y + button_height/2 - play_button_text.get_height()/2))
+        # Update the display
+        pygame.display.update()
 
-		settings_button_text = font.render("Settings", True, (255, 255, 255))
-		screen.blit(settings_button_text, (settings_button_rect.x + button_width/2 - settings_button_text.get_width()/2, settings_button_rect.y + button_height/2 - settings_button_text.get_height()/2))
+    pygame.quit()
 
-		credits_button_text = font.render("Credits", True, (255, 255, 255))
-		screen.blit(credits_button_text, (credits_button_rect.x + button_width/2 - credits_button_text.get_width()/2, credits_button_rect.y + button_height/2 - credits_button_text.get_height()/2))
+# Define the button rectangles
+play_button_rect = pygame.Rect(play_button_pos[0], play_button_pos[1], BUTTON_WIDTH, BUTTON_HEIGHT)
+settings_button_rect = pygame.Rect(settings_button_pos[0], settings_button_pos[1], BUTTON_WIDTH, BUTTON_HEIGHT)
+credits_button_rect = pygame.Rect(credits_button_pos[0], credits_button_pos[1], BUTTON_WIDTH, BUTTON_HEIGHT)
 
-		# Update display
-		pygame.display.flip()
+# Load button images
+play_button_img = pygame.image.load("play_button.png")
+settings_button_img = pygame.image.load("settings_button.png")
+credits_button_img = pygame.image.load("credits_button.png")
 
-# Handle error
-except Exception as e:
-	# Handle exception by closing game window and displaying error message.
-	pygame.quit()
-	print(f"An error occurred: {e}")
-
-# Quit Pygame
-pygame.quit(); print("Game successfully closed.")
+# Start the main menu loop
+main_menu()
